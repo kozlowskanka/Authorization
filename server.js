1cf5246475eddfe7437404bca845df70586fb585
Anka Kozłowska <kozlowskanka@gmail.com>
2020-05-07 17:12:46 +0200

    Add passport and Auth0

--- server.js
+++ server.js
@@ -2,8 +2,29 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 
 const app = express();
+
+passport.use(new GoogleStrategy({
+  clientID: '367884202943-mt6phu02mv7brahn2m99mjdjllc469tg.apps.googleusercontent.com',
+  clientSecret: 'e9BPjKqK0u4PtsQxTtVGSwcA',
+  callbackURL: 'http://localhost:8000/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+done(null, profile);
+}));
+
+// serialize user when saving to session
+passport.serializeUser((user, serialize) => {
+  serialize(null, user);
+});
+
+// deserialize user when reading from session
+passport.deserializeUser((obj, deserialize) => {
+  deserialize(null, obj);
+});
 
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
@@ -12,6 +33,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(session({ secret: 'anything' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -25,6 +50,9 @@ app.get('/user/no-permission', (req, res) => {
   res.render('noPermission');
 });
 
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile'] }));
+  
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
 });
@@ -32,3 +60,9 @@ app.use('/', (req, res) => {
 app.listen('8000', () => {
   console.log('Server is running on port: 8000');
 });
+
+// identyfikator klienta
+// 367884202943-mt6phu02mv7brahn2m99mjdjllc469tg.apps.googleusercontent.com
+
+// klucz klienta
+// e9BPjKqK0u4PtsQxTtVGSwcA
