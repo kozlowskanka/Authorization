df879b99371703303bb9281cca70890babb98916
Anka Kozłowska <kozlowskanka@gmail.com>
2020-05-09 13:38:14 +0200

    Add new user endpoints

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/logged', (req, res) => {
-  res.render('logged');
-});
-
 router.get('/no-permission', (req, res) => {
   res.render('noPermission');
 });
 
+router.get('/logged', (req, res) => {
+  if(req.user) {
+    res.render('logged');
+  } else {
+    res.redirect('../user/no-permission');
+  }
+});
+
+router.get('/profile', (req, res) => {
+  if(req.user) {
+    res.render('profile');
+  } else {
+    res.redirect('../user/no-permission');
+  }
+});
+
+router.get('/profile/settings', (req, res) => {
+  if(req.user) {
+    res.render('settings');
+  } else {
+    res.redirect('/user/no-permission');
+  }
+});
 
 module.exports = router;
